2df8ec9ecb51f13396f354c280529ed13d558249
KasperAPedersen <35560445+KasperAPedersen@users.noreply.github.com>
2023-11-08 17:56:23 +0100

    Update
    Changed default spot

--- spots.js
+++ spots.js
@@ -1,9 +1,9 @@
 module.exports = {
-    current: ["Ticketbox", "Jumpthrow", "inc/styling/images/tmp_spot1.jpg", "inc/styling/images/tmp_aim.jpg", "Stand here", "Right click throw"],
+    current: ["Ticketbox", "Jumpthrow", "inc/styling/images/tt.jpg", "inc/styling/images/tmp_aim.jpg", "Stand here", "Right click throw"],
     
     // ["TEAM", "NAME", "DESC", "VIEW NAME", "VIEW DESC", "PIC", "PIC", "POS", "ACTION"]
     mirage: [
-        ["tt", "short", "Smokes short from TT spawn", "Short", "Jumpthrow", "inc/styling/images/tt_to_short_pos.jpg", "inc/styling/images/tt_to_short_aim.jpg", "Stand here", "Jumpthrow"],
+        ["tt", "short", "Smokes short from TT spawn", "Short", "Jumpthrow", "inc/styling/images/tt.jpg", "inc/styling/images/tt_to_short_aim.jpg", "Stand here", "Jumpthrow"],
         ["tt", "window", "Smokes window from TT spawn", "Window", "Hold D & jumpthrow", "inc/styling/images/tt_to_window_pos.jpg", "inc/styling/images/tt_to_window_aim.jpg", "Stand here", "Hold D & jumpthrow"],
         ["tt", "stairs", "Smokes stairs from TT spawn", "Stairs", "Throw", "inc/styling/images/tt_to_stairs_pos.jpg", "inc/styling/images/tt_to_stairs_aim.jpg", "Stand here", "Throw"],
         ["tt", "jungle", "Smokes jungle from TT spawn", "Jungle", "Jump throw", "inc/styling/images/tt_to_jungle_pos.jpg", "inc/styling/images/tt_to_jungle_aim.jpg", "Stand here", "Jump throw"],
