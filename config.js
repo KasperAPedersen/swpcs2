97a487553c2c58cce829f186acdf7a0f433f020c
KasperAPedersen <35560445+KasperAPedersen@users.noreply.github.com>
2023-11-08 18:20:02 +0100

    Update

--- config.js
+++ config.js
@@ -1,14 +1,9 @@
 module.exports = {
-    username: "swoopsy045",
-    password: ".Z#w2/zfvyWxZLdS",
+    username: "",
+    password: "",
     usePort: 3000,
     nickname: "SWP CS2",
     autoLogin: false,
     url: `http://localhost`,
     refreshTimer: 5000
 }
-
-/*
-swoopsy045:.Z#w2/zfvyWxZLdS
-swoopsy046:SW'~77*vum`+74k`
-*/
