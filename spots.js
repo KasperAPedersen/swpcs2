229540af218eee6f4c114fa2ff454d454b2b0a2a
KasperAPedersen <35560445+KasperAPedersen@users.noreply.github.com>
2023-11-08 16:02:57 +0100

    Update
    Added action instructions

--- spots.js
+++ spots.js
@@ -1,14 +1,14 @@
 module.exports = {
-    current: ["Ticketbox", "Jumpthrow", "inc/styling/images/tmp_spot1.jpg", "inc/styling/images/tmp_aim.jpg"],
+    current: ["Ticketbox", "Jumpthrow", "inc/styling/images/tmp_spot1.jpg", "inc/styling/images/tmp_aim.jpg", "Stand here", "Right click throw"],
     
-    // ["TEAM", "NAME", "DESC", "VIEW NAME", "VIEW DESC", "PIC", "PIC"]
+    // ["TEAM", "NAME", "DESC", "VIEW NAME", "VIEW DESC", "PIC", "PIC", "POS", "ACTION"]
     mirage: [
-        ["tt", "ticketbox", "Smokes ticketbox from ramp", "Ticketbox", "Jumpthrow", "inc/styling/images/tmp_spot1.jpg", "inc/styling/images/tmp_aim.jpg"],
-        ["tt", "jungle", "Smokes Jungle from ramp", "Jungle", "Jumpthrow", "inc/styling/images/tmp_spot1.jpg", "inc/styling/images/tmp_aim.jpg"],
-        ["tt", "stairs", "Smokes Stairs from ramp", "Stairs", "Throw", "inc/styling/images/tmp_spot1.jpg", "inc/styling/images/tmp_aim.jpg"]
+        ["tt", "ticketbox", "Smokes ticketbox from ramp", "Ticketbox", "Jumpthrow", "inc/styling/images/tmp_spot1.jpg", "inc/styling/images/tmp_aim.jpg", "Stand here", "Right click throw"],
+        ["tt", "jungle", "Smokes Jungle from ramp", "Jungle", "Jumpthrow", "inc/styling/images/tmp_spot1.jpg", "inc/styling/images/tmp_aim.jpg", "Stand here", "Right click throw"],
+        ["tt", "stairs", "Smokes Stairs from ramp", "Stairs", "Throw", "inc/styling/images/tmp_spot1.jpg", "inc/styling/images/tmp_aim.jpg", "Stand here", "Right click throw"]
     ],
     overpass: [
-        ["tt", "test", "Smokes ticketbox from ramp", "Ticketbox", "Jumpthrow", "inc/styling/images/tmp_spot1.jpg", "inc/styling/images/tmp_aim.jpg"],
-        ["tt", "test2", "Smokes Jungle from ramp", "Jungle", "Jumpthrow", "inc/styling/images/tmp_spot1.jpg", "inc/styling/images/tmp_aim.jpg"]
+        ["tt", "test", "Smokes ticketbox from ramp", "Ticketbox", "Jumpthrow", "inc/styling/images/tmp_spot1.jpg", "inc/styling/images/tmp_aim.jpg", "Stand here", "Right click throw"],
+        ["tt", "test2", "Smokes Jungle from ramp", "Jungle", "Jumpthrow", "inc/styling/images/tmp_spot1.jpg", "inc/styling/images/tmp_aim.jpg", "Stand here", "Right click throw"]
     ]
 };
