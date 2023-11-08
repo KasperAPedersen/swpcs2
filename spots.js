3ac548849453a17cccbfe9c38a5f18b741fe8e21
KasperAPedersen <35560445+KasperAPedersen@users.noreply.github.com>
2023-11-09 00:24:37 +0100

    Update
    Added smokes

--- spots.js
+++ spots.js
@@ -1,20 +1,32 @@
 module.exports = {
-    current: ["Ticketbox", "Jump throw", "inc/styling/images/tt_to_ticketbox_pos.jpg", "inc/styling/images/tt_to_ticketbox_aim.jpg", "Stand here", "Jump throw"],
+    current: ["Ticketbox", "Jump throw", "inc/styling/images/mirage/a/tt_to_ticketbox_pos.jpg", "inc/styling/images/mirage/a/tt_to_ticketbox_aim.jpg", "Stand here", "Jump throw"],
     
     // ["TEAM", "NAME", "DESC", "VIEW NAME", "VIEW DESC", "PIC", "PIC", "POS", "ACTION"]
     mirage: [
-        ["tt", "bshort", "Smokes short from apartment", "Short", "Jump throw", "inc/styling/images/tt_to_b_short_pos.jpg", "inc/styling/images/tt_to_b_short_aim.jpg", "Stand here", "Jump throw"],
-        ["tt", "bwindow", "Smokes market window from apartment", "Market Window", "jumpthrow", "inc/styling/images/tt_to_b_market_window_pos.jpg", "inc/styling/images/tt_to_b_market_window_aim.jpg", "Stand here", "jumpthrow"],
-        ["tt", "bdoor", "Smokes market door from apartment", "Door", "Jump throw", "inc/styling/images/tt_to_b_market_door_pos.jpg", "inc/styling/images/tt_to_b_market_door_aim.jpg", "Stand here", "Jump throw"],
+        // b
+        ["tt", "bshort", "Smokes short from apartment", "Short", "Jump throw", "inc/styling/images/mirage/b/tt_to_b_short_pos.jpg", "inc/styling/images/mirage/b/tt_to_b_short_aim.jpg", "Stand here", "Jump throw"],
+        ["tt", "bwindow", "Smokes market window from apartment", "Market Window", "jumpthrow", "inc/styling/images/mirage/b/tt_to_b_market_window_pos.jpg", "inc/styling/images/mirage/b/tt_to_b_market_window_aim.jpg", "Stand here", "jumpthrow"],
+        ["tt", "bdoor", "Smokes market door from apartment", "Door", "Jump throw", "inc/styling/images/mirage/b/tt_to_b_market_door_pos.jpg", "inc/styling/images/mirage/b/tt_to_b_market_door_aim.jpg", "Stand here", "Jump throw"],
         
-        ["tt", "mshort", "Smokes short from TT spawn", "Short", "Jumpthrow", "inc/styling/images/tt_to_short_pos.jpg", "inc/styling/images/tt_to_short_aim.jpg", "Stand here", "Jumpthrow"],
-        ["tt", "mwindow", "Smokes window from TT spawn", "Window", "Hold D & jumpthrow", "inc/styling/images/tt_to_window_pos.jpg", "inc/styling/images/tt_to_window_aim.jpg", "Stand here", "Hold D & jumpthrow"],
-        ["tt", "astairs", "Smokes stairs from TT spawn", "Stairs", "Throw", "inc/styling/images/tt_to_stairs_pos.jpg", "inc/styling/images/tt_to_stairs_aim.jpg", "Stand here", "Throw"],
-        ["tt", "ajungle", "Smokes jungle from TT spawn", "Jungle", "Jump throw", "inc/styling/images/tt_to_jungle_pos.jpg", "inc/styling/images/tt_to_jungle_aim.jpg", "Stand here", "Jump throw"],
-        ["tt", "aticketbox", "Smokes ticketbox from TT spawn", "Ticketbox", "Jump throw", "inc/styling/images/tt_to_ticketbox_pos.jpg", "inc/styling/images/tt_to_ticketbox_aim.jpg", "Stand here", "Jump throw"]
+        // mid
+        ["tt", "mshort", "Smokes short from TT spawn", "Short", "Jumpthrow", "inc/styling/images/mirage/mid/tt_to_short_pos.jpg", "inc/styling/images/mirage/mid/tt_to_short_aim.jpg", "Stand here", "Jumpthrow"],
+        ["tt", "mwindow", "Smokes window from TT spawn", "Window", "Hold D & jumpthrow", "inc/styling/images/mirage/mid/tt_to_window_pos.jpg", "inc/styling/images/mirage/mid/tt_to_window_aim.jpg", "Stand here", "Hold D & jumpthrow"],
+       
+        // a
+        ["tt", "astairs", "Smokes stairs from TT spawn", "Stairs", "Throw", "inc/styling/images/mirage/a/tt_to_stairs_pos.jpg", "inc/styling/images/mirage/a/tt_to_stairs_aim.jpg", "Stand here", "Throw"],
+        ["tt", "ajungle", "Smokes jungle from TT spawn", "Jungle", "Jump throw", "inc/styling/images/mirage/a/tt_to_jungle_pos.jpg", "inc/styling/images/mirage/a/tt_to_jungle_aim.jpg", "Stand here", "Jump throw"],
+        ["tt", "aticketbox", "Smokes ticketbox from TT spawn", "Ticketbox", "Jump throw", "inc/styling/images/mirage/a/tt_to_ticketbox_pos.jpg", "inc/styling/images/mirage/a/tt_to_ticketbox_aim.jpg", "Stand here", "Jump throw"]
     ],
     overpass: [
-        ["tt", "test", "Smokes ticketbox from ramp", "Ticketbox", "Jumpthrow", "inc/styling/images/tmp_spot1.jpg", "inc/styling/images/tmp_aim.jpg", "Stand here", "Right click throw"],
-        ["tt", "test2", "Smokes Jungle from ramp", "Jungle", "Jumpthrow", "inc/styling/images/tmp_spot1.jpg", "inc/styling/images/tmp_aim.jpg", "Stand here", "Right click throw"]
+        // b
+        ["tt", "bheaven", "Smokes heaven from water/short", "Heaven", "Jump throw", "inc/styling/images/overpass/b/tt_to_b_heaven_pos.jpg", "inc/styling/images/overpass/b/tt_to_b_heaven_aim.jpg", "Stand here", "Jump throw"],
+        ["tt", "bgraffiti", "Smokes graffiti from water/short", "Graffiti", "Left throw", "inc/styling/images/overpass/b/tt_to_b_graffiti_pos.jpg", "inc/styling/images/overpass/b/tt_to_b_graffiti_aim.jpg", "Stand here", "Left throw"],
+        ["tt", "bdildo", "Smokes right side of dildo from water/short", "Dildo", "Left throw", "inc/styling/images/overpass/b/dildoright_pos.jpg", "inc/styling/images/overpass/b/dildoright_aim.jpg", "Stand here", "Left throw"],
+        ["tt", "bdildo1", "Smokes left side of dildo from water/short", "Dildo", "Left throw", "inc/styling/images/overpass/b/dildoleft_pos.jpg", "inc/styling/images/overpass/b/dildoleft_aim.jpg", "Stand here", "Left throw"],
+        
+        // a
+        ["tt", "abank", "Smokes bank from top of connector", "Bank", "Jump throw", "inc/styling/images/overpass/a/bank_pos.jpg", "inc/styling/images/overpass/a/bank_aim.jpg", "Stand here", "Jump throw"],
+        ["tt", "astairs", "Smokes CT spawn stairs from top of connector", "CT Stairs", "Jump throw", "inc/styling/images/overpass/a/ctstairs_pos.jpg", "inc/styling/images/overpass/a/ctstairs_aim.jpg", "Stand here", "Jump throw"]
+        
     ]
 };
